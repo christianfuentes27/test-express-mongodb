29d560ddc164c570c3962630ac87c04a88bf79e4
Christian <cjfuentes27@ieszaidinvergeles.org>
2022-12-23 17:09:24 +0100

    Saving data from postman into mongo db

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 var bodyParser = require('body-parser');
-const app = express();
 var mongoose = require("mongoose");
+
+const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://mongoadmin:secret@localhost:1888/?authMechanism=DEFAULT");
+
 var nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String
+    name: String,
+    lastname: String
 });
 var User = mongoose.model("User", nameSchema);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log("El servidor está inicializado en el puerto 3000");
+});
+
+app.get('/', function(req, res) {
+    res.send('hola');
 });
 
 app.post('/test', function (req, res) {
@@ -25,5 +32,4 @@ app.post('/test', function (req, res) {
         .catch(err => {
             res.status(400).send("unable to save to database");
         });
-    // res.send(data);
 });
